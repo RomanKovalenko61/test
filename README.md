d8c958dbe25f304841c6696cbf2f35f43e04e50c
Roman <astek14@mail.ru>
2024-12-19 07:26:10 +0300

    add super code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # test
 ## testing
+
+Set Java 8 into IDEA File/Project structure
+
+Mark src/main/java  
+Mark src/test/java  
---
+++ src/main/java/ru/example/Calc.java
@@ -0,0 +1,8 @@
+package ru.example;
+
+public class Calc {
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
